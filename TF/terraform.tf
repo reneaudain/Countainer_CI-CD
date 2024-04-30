f5c13f100ad56ab24851d1ddea82c7be966a0dc9
Rene Audain <155379555+reneaudain@users.noreply.github.com>
2024-04-30 16:46:42 -0500

    Update terraform.tf

--- TF/terraform.tf
+++ TF/terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.47.0"
+    }
+  }
+
   backend "remote" {
     organization = "GitHub-Actions_CICD"
 
@@ -7,4 +14,7 @@ terraform {
       #tags = ["some-tag"]
     }
   }
+}
+provider "aws" {
+  region = var.region
 }
