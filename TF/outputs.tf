a900b216816fbfa06d15cfcd0423b9dcd7b759d8
Rene Audain <155379555+reneaudain@users.noreply.github.com>
2024-04-30 22:15:02 -0500

    Update outputs.tf

--- TF/outputs.tf
+++ TF/outputs.tf
@@ -9,7 +9,23 @@ output "ssh_permissions" {
   description = "permisions command for key pair"
   value = "chmod 400 ${aws_key_pair.generated_key.key_name}"
 }
-output "user_key" {
-  value = aws_iam_access_key.git_key
+output "user_id" {
+  value = aws_iam_access_key.git_key.id
+  description = "Access Id"
   sensitive = true
+}
+output "user_secret" {
+  description = "Secret key"
+  sensitive = true
+  value = aws_iam_access_key.git_key.secret
+}
+output "key_name" {
+  value = aws_key_pair.generated_key.key_name
+  sensitive = true
+  description = "Instance Key Pair name"
+}
+output "EC2_PEM_KEY" {
+  description = "Key Pair Value"
+  sensitive = true
+  value = aws_key_pair.generated_key.public_key
 }
